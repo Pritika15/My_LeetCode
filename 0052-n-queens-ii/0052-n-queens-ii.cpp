f2aae68fd2ebe9f955a35a49159c80b4caa5ad06
Pritika Sharma <psharma6_be20@thapar.edu>
2023-01-14 12:19:52 +0530

    Time: 5 ms (67.81%), Space: 6.8 MB (28.17%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string>&board,int n)
+    {
+        int r=row;
+        int c=col;
+        while(r>=0 && c>=0)
+        {
+            if(board[r][c]=='Q') return false;
+            r--;
+            c--;
+        }
+        r=row;
+        c=col;
+        while(c>=0)
+        {
+             if(board[r][c]=='Q') return false;
+            c--;
+        }
+        r=row;
+        c=col;
+        while(c>=0 && r<n)
+        {
+            if(board[r][c]=='Q') return false;
+            r++;
+            c--;
+        }
+        return true;
+    }
+    void Num(int col, vector<vector<string>>&ans,vector<string>&board,int n,int &count)
+    {
+        
+        if(col==n)
+        {
+            ans.push_back(board);
+            count++;
+            return;
+            
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(isSafe(i,col,board,n))
+            {
+                board[i][col]='Q';
+                Num(col+1,ans,board,n,count);
+                board[i][col]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+        {
+            board[i]=s;
+        }
+        int count=0;
+        Num(0,ans,board,n,count);
+        return count;
+    }
+};
