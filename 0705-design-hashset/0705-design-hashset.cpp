c1f7ffec439c3ba516f2df2c8a9fd269a655952e
Pritika Sharma <psharma6_be20@thapar.edu>
2023-05-30 18:12:53 +0530

    Time: 115 ms (45.48%), Space: 41.8 MB (58.80%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,42 @@
 class MyHashSet {
 public:
-    vector<int>vec;
+    vector<list<int>>vec;
     int size;
     MyHashSet() {
-        size=1000002;
+        // size=1000002;
+        // vec.resize(size);
+        size=1000;
         vec.resize(size);
     }
+    int hash(int key)
+    {
+        return key%size;
+    }
     
+    list<int>:: iterator search(int key)
+    {
+        int i=hash(key);
+        return find(vec[i].begin(),vec[i].end(),key);
+    }
     void add(int key) {
-        vec[key]=1;
+        // vec[key]=1;
+        if(contains(key)) return;
+        int i=hash(key);
+        vec[i].push_back(key);
     }
     
     void remove(int key) {
-        vec[key]=0;
+        if(!contains(key)) return;
+        int i=hash(key);
+        vec[i].erase(search(key));
     }
     
     bool contains(int key) {
-        return vec[key];
+        // return vec[key];
+        int i=hash(key);
+        if(search(key)!= vec[i].end()) return true;
+        else
+            return false;
     }
 };
 
