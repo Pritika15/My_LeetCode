552b27228d9f0adf2f8b11a70d6b228b3c29dd06
Pritika Sharma <psharma6_be20@thapar.edu>
2023-06-01 21:54:09 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (15.32%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,36 +11,40 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-         vector<int> ans;
-        TreeNode* curr=root;
-        while(curr!=NULL)
+    void solve(TreeNode* node,vector<int>&ans,stack<TreeNode*>&s)
+    {
+        s.push(node);
+        if(node->left!=NULL)
+            solve(node->left,ans,s);
+        if(node->left==NULL && !s.empty() )
         {
-            if(curr->left==NULL)
-            {
-                ans.push_back(curr->val);
-                curr=curr->right;
-            }
-            else
-            {
-                TreeNode* prev= curr->left;
-                while(prev->right && prev->right != curr)
-                {
-                    prev=prev->right;
-                }
-                if(prev->right==NULL)
-                {
-                    prev->right=curr;
-                    curr=curr->left;
-                }
-                else
-                {
-                    prev->right=NULL;
-                    ans.push_back(curr->val);
-                    curr=curr->right;
-                }
-            }
+           int x=s.top()->val;
+            ans.push_back(x);
+            s.pop();
         }
+        if(node->right!=NULL)
+        {
+            solve(node->right,ans,s);
+        }
+       if(node->right==NULL && !s.empty())
+        {
+           int x=s.top()->val;
+            ans.push_back(x);
+            s.pop();
+        }
+        
+            
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+      //iterative inorder trvaersal
+        vector<int> ans;
+        if(root==NULL)
+            return ans;
+            
+        
+        stack<TreeNode*>s;
+        solve(root,ans,s);
         return ans;
+        
     }
 };
