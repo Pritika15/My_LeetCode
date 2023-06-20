344f91409324e9be099d9dcc80b95cf186338c36
Pritika Sharma <psharma6_be20@thapar.edu>
2023-06-21 02:07:19 +0530

    Time: 11 ms (88.31%), Space: 15.8 MB (94.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,17 +2,40 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
     vector<int>ans;
-    unordered_map<int,int>m;
-    int n=nums.size();
-    int v=floor(n/3);
-    for(int i=0;i<n;i++)
+    int num1=INT_MIN, num2=INT_MIN;
+    int c1=0,c2=0;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size();i++)
     {
-        m[nums[i]]++;
+        if(nums[i]==num1)c1++;
+        else if(nums[i]==num2)c2++;
+        else if(c1==0)
+        {
+            num1=nums[i];
+            c1=1;
+            
+         }
+        else if(c2==0)
+        {
+            num2=nums[i];
+            c2=1;
+        }
+        else
+        {
+            c1--;
+            c2--;
+        }
     }
-    for(auto itr:m)
+    int CNT1=0, CNT2=0;
+    for(int i=0;i<nums.size();i++)
     {
-        if(itr.second>v) ans.push_back(itr.first);
+        if(nums[i]==num1) CNT1++;
+        if(nums[i]==num2) CNT2++;
+        
     }
+    int Check=floor(nums.size()/3);
+    if(CNT1>Check) ans.push_back(num1);
+    if(CNT2>Check) ans.push_back(num2);
     return ans;
     }
 };
