599dec606f597bb89109cf695a127b15d47806a2
Pritika Sharma <psharma6_be20@thapar.edu>
2023-06-22 23:30:51 +0530

    Added solution - LeetHub

---
+++ Power Set - GFG/power-set.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		    vector<string> ans;
+		    int n=s.length();
+		    for(int i=0;i<(1<<n);i++)
+		    {
+		        string substr="";
+		        for(int j=0;j<n;j++)
+		        {
+		            if(i & 1<<j)
+		            {
+		                substr+=s[j];
+		            }
+		            
+		             
+		        }
+		        if(substr.length()>0) ans.push_back(substr);
+		       
+		    }
+		    sort(ans.begin(),ans.end());
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
