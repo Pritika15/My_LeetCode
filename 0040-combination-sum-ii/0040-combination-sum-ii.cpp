89544dcb7ccdd091f37662d95fcdeebb1286683c
Pritika Sharma <psharma6_be20@thapar.edu>
2023-06-27 00:33:01 +0530

    Time: 6 ms (63.94%), Space: 10.9 MB (27.84%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-//     void helper(int index,vector<int>& candidates, int target,vector<int>&v,vector<vector<int>>&ans)
-//     {
-//         if(target==0) ans.push_back(v);
-        
-//         for(int i=index;i<candidates.size();i++)
-//         {
-//             if(i!=index && candidates[i]==candidates[i-1] ) continue;
-//             if(candidates[i]>target) break;
-//             v.push_back(candidates[i]);
-//             helper(index+1,candidates,target-candidates[i],v,ans);
-//             v.pop_back();
-//         }
-//     }
-    
-    void f(vector<int> &arr, int target, int idx, vector<int>ds, vector<vector<int>>& ans){
-        if(target==0){
-            ans.push_back(ds);
-        }
-        for(int i = idx; i<arr.size(); i++){
-            if(i!= idx and arr[i]==arr[i-1])    continue;
-            if(arr[i]>target)   break;
-            ds.push_back(arr[i]);
-            f(arr, target-arr[i], i+1, ds, ans);
-            ds.pop_back();
-        }
-    }
+void solve(int indx,vector<int>&v,set<vector<int>>&ans, vector<int> &arr, int n, int target)
+{
+	if(target==0)
+	{
+		ans.insert(v);
+		return;
+	}
+	for(int i=indx;i<n;i++)
+	{
+		if(i>indx && arr[i]==arr[i-1]) continue;
+		if(arr[i]>target) break;
+		 v.push_back(arr[i]);
+		solve(i+1,v,ans,arr,n,target-arr[i]);
+		v.pop_back();
+		
+	}
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-//         sort(candidates.begin(),candidates.end());
-//         vector<vector<int>> ans;
-//         vector<int>v;
-        
-//         helper(0,candidates,target,v,ans);
-//         return ans;
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
-        vector<int > ds;
-        f(candidates,target, 0, ds, ans);
-        return ans;
+    
+    set<vector<int>>ans;
+    vector<vector<int>>a;
+	vector<int>v;
+    int n=candidates.size();
+	sort(candidates.begin(),candidates.end());
+	solve(0,v,ans,candidates,n,target);
+    for(auto itr:ans)
+    {
+        a.push_back(itr);
+    }
+	return a;
     }
 };
