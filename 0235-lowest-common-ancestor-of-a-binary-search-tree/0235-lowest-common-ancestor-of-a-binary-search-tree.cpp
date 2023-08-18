56e4ec3edc3f7b8536ffe543a6cebc931bbf82b8
Pritika Sharma <psharma6_be20@thapar.edu>
2023-08-18 18:01:31 +0530

    Time: 33 ms (44.08%), Space: 23.4 MB (16.59%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,50 +10,16 @@
 
 class Solution {
 public:
-    bool inorder(TreeNode* root, TreeNode* target, vector<TreeNode*>&temp)
-    {
-        if(!root) return false;
-        temp.push_back(root);
-        if(root==target) return true;
-        if((inorder(root->left,target,temp))|| (inorder(root->right,target,temp))==true)
-            return true;
-        temp.pop_back();
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>path1;
-        vector<TreeNode*>path2;
-        inorder(root,p,path1);
-        inorder(root,q,path2);
-        int n=path1.size();
-        int m=path2.size();
-        TreeNode* ans;
-        if(n<m)
-        {
-             for(int i=0;i<n;i++)
-             {
-                 if(path1[i]==path2[i])
-                 {
-                     ans=path1[i];
-                 }
-                 else if(path1[i]!=path2[i])
-                     break;
-             }
-        }
-        else
-        {
-           for(int i=0;i<m;i++)
-             {
-                 if(path1[i]==path2[i])
-                 {
-                     ans=path1[i];
-                 }
-                 else if(path1[i]!=path2[i])
-                     break;
-             } 
-        }
-        return ans;
-       
+        if(root==NULL || root==p || root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
         
+        if(!left)
+        {
+            return right;
+        }
+        else if(!right) return left;
+        else return root;
     }
 };
