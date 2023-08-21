cb1b1fda93abc9a75386a91e81c1604fc119e9ad
Pritika Sharma <psharma6_be20@thapar.edu>
2023-08-21 23:27:31 +0530

    Time: 12 ms (92.74%), Space: 13.5 MB (57.60%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string str=s;
+         int i = 1, j = 0, n = str.size();
+        vector<int> dp(n+1,0);
+        while( i < str.size() ){
+            if( str[i] == str[j] ) dp[++i]=++j;
+            else if( j == 0 ) i++;
+            else j = dp[j];
+        }
+        return dp[n]&&dp[n]%(n-dp[n])==0;
+    }
+};
