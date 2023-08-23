ad9175ecac165d0b3091a9998e6ec50cdd373beb
Pritika Sharma <psharma6_be20@thapar.edu>
2023-08-23 13:41:08 +0530

    Time: 36 ms (98.82%), Space: 41.2 MB (63.41%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-    unordered_map<int,int>m;
-    for(int i=0;i<nums.size();i++)
-    {
-      m[nums[i]]++;
-    }
-    int ans=0;
-    for(int i=1;i<=1e5+1;i++)
-    {
-      if(m.find(i)==m.end())
-      {
-        ans=i;
-        break;
-        
-      }
-    }
-    return ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            int ele=nums[i];
+           
+            if(ele>0 && ele<n)
+            {
+                 int chair=ele-1;   
+                if(nums[chair]!=ele)
+                {
+                    swap(nums[chair],nums[i]);
+                    i--;
+                }
+            }
+            
+        }
+       
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+            {
+               return i+1;
+            }
+        }
+        return n+1;
     }
 };
