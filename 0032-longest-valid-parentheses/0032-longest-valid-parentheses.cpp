4edb085c2b7243500161446431a7e51587807224
Pritika Sharma <psharma6_be20@thapar.edu>
2023-08-23 12:46:55 +0530

    Time: 3 ms (81.62%), Space: 7.2 MB (65.25%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int maxi=0;
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if(st.empty())
+                {
+                    st.push(i);
+                    
+                }
+                else
+                {
+                    maxi=max(maxi,i-st.top());
+                }
+            }
+        }
+        return maxi;
+    }
+};
