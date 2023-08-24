3602c35f500d846a313ed276e6e17cb406849eca
Pritika Sharma <psharma6_be20@thapar.edu>
2023-08-25 00:10:21 +0530

    Added solution - LeetHub

--- Prerequisite Tasks - GFG/prerequisite-tasks.cpp
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -5,52 +5,46 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-	bool isPossible(int N, vector<pair<int, int> >& prerequisites) {
+	bool isPossible(int N,int P, vector<pair<int, int> >& prerequisites) {
 	    // Code here
-	     
-        vector<int>vis(N,0);
-        
-        vector<int>AdjList[N];
-        //BFS-->Kahns
-        queue<int>q;
-        for(int i=0;i<prerequisites.size();i++)
-        {
-            int x=prerequisites[i].first;
-            int y=prerequisites[i].second;
-            AdjList[x].push_back(y);
-        }
-        
-        vector<int>Indegree(N,0);
-        for(int i=0;i<N;i++)
-        {
-            for(auto itr:AdjList[i])
-            {
-                Indegree[itr]++;
-            }
-        }
-        
-        for(int i=0;i<Indegree.size();i++)
-        {
-            if(Indegree[i]==0)
-            {
-                q.push(i);
-            }
-        }
-        int cnt=0;
-        while(!q.empty())
-        {
-            int xx=q.front();
-            q.pop();
-            cnt++;
-            for(auto it:AdjList[xx])
-            {
-                Indegree[it]--;
-                if(Indegree[it]==0) q.push(it);
-            }
-        }
-        if(cnt!=N){
-            return  false;}
-        return true;
+	    vector<int>AdjList[N];
+	    for(int i=0;i<prerequisites.size();i++)
+	    {
+	        int u=prerequisites[i].first;
+	        int v=prerequisites[i].second;
+	        AdjList[u].push_back(v);
+	    }
+	    
+	    vector<int>Indeg(N,0);
+	    for(int i=0;i<N;i++)
+	    {
+	        for(auto it:AdjList[i])
+	        {
+	            Indeg[it]++;
+	        }
+	        
+	    }
+	    
+	    stack<int>st;
+	    for(int i=0;i<N;i++)
+	    {
+	        if(Indeg[i]==0) st.push(i);
+	    }
+	    vector<int>v;
+	    while(!st.empty())
+	    {
+	        int node=st.top();
+	        st.pop();
+	        v.push_back(node);
+	        for(auto it:AdjList[node])
+	        {
+	            Indeg[it]--;
+	            if(Indeg[it]==0) st.push(it);
+	        }
+	    }
+	    
+	    if(v.size()==N) return true;
+	    return false;
 	}
 };
 
@@ -71,7 +65,7 @@ int main(){
         // string s;
         // cin>>s;
         Solution ob;
-        if (ob.isPossible(N, prerequisites))
+        if (ob.isPossible(N,P, prerequisites))
             cout << "Yes";
         else
             cout << "No";
