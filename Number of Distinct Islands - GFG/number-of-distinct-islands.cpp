a74e8ca1321b899fbc2cd9222f5c5c9e33c92f55
Pritika Sharma <psharma6_be20@thapar.edu>
2023-12-16 17:45:22 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -9,51 +9,58 @@ using namespace std;
 
 class Solution {
   public:
-  void dfs(int r, int c,vector<vector<int>>& grid,vector<vector<int>>&vis,vector<pair<int,int>>&vec,int baseR,int baseC)
-  {
-      vis[r][c]=1;
-      vec.push_back({r-baseR,c-baseC});
-      int n=grid.size();
-      int m=grid[0].size();
-      
-      for(int i=-1;i<=1;i++)
-      {
-          for(int j=-1;j<=1;j++)
-          {
-              if(abs(i)!=abs(j))
-              {
-                  int Newr=r+i;
-                  int Newc=c+j;
-                  
-                  if(Newr>=0 && Newr<n && Newc>=0 && Newc<m && !vis[Newr][Newc] && grid[Newr][Newc]==1 )
-                  {
-                      dfs(Newr,Newc,grid,vis,vec,baseR,baseC);
-                  }
-              }
-          }
-      }
-  }
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        
-        set<vector<pair<int,int>>> st;
-        
+        queue<pair<int,int>>q;
+        map<vector<pair<int,int>>,int>AllCombs;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!vis[i][j] && grid[i][j]==1)
+                if(grid[i][j]==1 && !vis[i][j])
                 {
-                   vector<pair<int,int>>vec;
-                   dfs(i,j,grid,vis,vec,i,j);
-                   st.insert(vec);
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    vector<pair<int,int>>v;
+                    while(!q.empty())
+                    {
+                        int r=q.front().first;
+                        int c=q.front().second;
+                        q.pop();
+                        v.push_back({r-i,c-j});
+                        for(int i= -1;i<=1;i++)
+                        {
+                            for(int j= -1;j<=1;j++)
+                            {
+                                if(abs(i)!=abs(j))
+                                {
+                                    int newr=r+i;
+                                    int newc=c+j;
+                                    
+                                    if(newr>=0 && newc>=0 && newr<n && newc<m && grid[newr][newc]==1 && !vis[newr][newc])
+                                    {
+                                        q.push({newr,newc});
+                                        vis[newr][newc]=1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if(AllCombs.find(v)==AllCombs.end())
+                    {
+                        AllCombs.insert({v,1});
+                    }
+                    else
+                    AllCombs[v]++;
                 }
             }
+            
         }
-        return st.size();
+        return AllCombs.size();
+        
     }
 };
 
