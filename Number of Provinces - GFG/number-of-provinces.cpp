d8830d9ad1a39153c024a64cbd1515620b7967f6
Pritika Sharma <psharma6_be20@thapar.edu>
2023-12-16 16:10:23 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -5,71 +5,45 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-class DisjointSet
-{
-    public:
-    vector<int>parent,size;
-    DisjointSet(int n)
-    {
-        size.resize(n+1,1);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++) parent[i]=i;
-    }
-    
-    int findParent(int node)
-    {
-        if(node==parent[node]) return node;
-        return parent[node]=findParent(parent[node]);
-    }
-    
-    void UnionBySize(int u, int v)
-    {
-        int ulp_u=findParent(u);
-        int ulp_v=findParent(v);
-        if(ulp_u==ulp_v) return;
-        if(size[ulp_u]>size[ulp_v])
-        {
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
-        else
-        {
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-        }
-    }
-    
-};
+
 class Solution {
   public:
+  void dfs(int curr,vector<int>&vis,vector<int>adjList[])
+  {
+      vis[curr]=1;
+      for(auto itr:adjList[curr])
+      {
+          if(!vis[itr])
+          {
+              dfs(itr,vis,adjList);
+          }
+      }
+  }
     int numProvinces(vector<vector<int>> adj, int V) {
         // code here
-        vector<vector<int>>graph;
+        vector<int>adjList[V+1];
         for(int i=0;i<adj.size();i++)
         {
-            for(int j=0;j<adj[i].size();j++)
+            for(int j=0;j<adj[0].size();j++)
             {
                 if(adj[i][j]==1)
                 {
-                    graph.push_back({i,j});
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
                 }
             }
         }
-        DisjointSet ds(V);
-        for(int i=0;i<graph.size();i++)
-        {
-            if(ds.findParent(graph[i][0])!=ds.findParent(graph[i][1]))
-            {
-                ds.UnionBySize(graph[i][0],graph[i][1]);
-            }
-        }
         int cnt=0;
-        for(int i=0;i<V;i++)
-        {
-            if(i==ds.parent[i]) cnt++;
-            
-        }
-        return cnt;
+        vector<int>vis(V,0);
+       for(int i=0;i<V;i++)
+       {
+           if(!vis[i])
+           {
+               dfs(i,vis,adjList);
+               cnt++;
+           }
+       }
+       return cnt;
     }
 };
 
