ffc9bfa77c59b2d4d4e7fc9c0ad3235e4ce97b08
Pritika Sharma <psharma6_be20@thapar.edu>
2023-12-17 17:42:41 +0530

    Time: 6 ms (29.61%), Space: 9.4 MB (76.30%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int i=0,j=0;
-    if(m==0)
-    {
-        nums1.clear();
+       int i=m-1,j=0;
+        while(i>=0 && j<n)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                swap(nums1[i],nums2[j]);
+                i--;
+                j++;
+            }
+            else
+            {
+                break;
+            }
+        }
         for(int i=0;i<n;i++)
-            nums1.push_back(nums2[i]);
-        return;
-    }
-    if(n==0) return ;
-	while(i<m)
-	{
-		if(nums1[i]>nums2[j])
-		{
-			int temp=nums1[i];
-			nums1[i]=nums2[j];
-			nums2[j]=temp;
-			sort(nums2.begin(),nums2.end());
-
-		}
-		i++;
-		
-	}
-	// sort(arr2.begin(),arr2.end());
-	while(j<n)
-	{
-		nums1[i]=nums2[j];
-		i++;
-		j++;
-
-	}
-	
+        {
+            nums1[m+i]=nums2[i];
+        }
+        sort(nums1.begin(),nums1.end());
     }
 };
