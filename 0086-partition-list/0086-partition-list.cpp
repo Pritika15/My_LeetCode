ea2936adfa1c5c365468ee92ddc071d24265e58b
Pritika Sharma <psharma6_be20@thapar.edu>
2023-12-25 00:26:27 +0530

    Time: 8 ms (9.49%), Space: 11.1 MB (7.60%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,30 +11,38 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode*temp=head;
+        if(!head || !head->next) return head;
+        
+        vector<int>v;
+        ListNode* temp=head;
+        while(temp)
+        {
+            v.push_back(temp->val);
+            
+            temp=temp->next;
+        }
+        
         vector<int>v1;
         vector<int>v2;
-        while(temp!=NULL)
+        for(int i=0;i<v.size();i++)
         {
-            if(temp->val<x) v1.push_back(temp->val);
-            else v2.push_back(temp->val);
-            temp=temp->next;
-            
+            if(v[i]<x)v1.push_back(v[i]);
+            else v2.push_back(v[i]);
         }
-        ListNode* Node=new ListNode(-1);
-         ListNode* N=Node;
-        
+        ListNode*ans=new ListNode(-1);
+        ListNode*a=ans;
         for(int i=0;i<v1.size();i++)
         {
-            N->next=new ListNode(v1[i]);
-            N=N->next;
+            ListNode*t=new ListNode(v1[i]);
+            ans->next=t;
+            ans=ans->next;
         }
         for(int i=0;i<v2.size();i++)
         {
-            N->next=new ListNode(v2[i]);
-            N=N->next;
+            ListNode*t=new ListNode(v2[i]);
+            ans->next=t;
+            ans=ans->next;
         }
-        return Node->next;
-        
+        return a->next;
     }
 };
