319c70b9d48d68d319b381f87b5f976db61d6db4
Pritika Sharma <psharma6_be20@thapar.edu>
2023-12-24 16:02:24 +0530

    Time: 1969 ms (5.00%), Space: 504.3 MB (5.00%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size(); //size of the array
+    set<vector<int>> st;
+
+    //checking all possible quadruplets:
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            set<long long> hashset;
+            for (int k = j + 1; k < n; k++) {
+                // taking bigger data type
+                // to avoid integer overflow:
+                long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long fourth = target - sum;
+                if (hashset.find(fourth) != hashset.end()) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], (int)(fourth)};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+                // put the kth element into the hashset:
+                hashset.insert(nums[k]);
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+    }
+};
