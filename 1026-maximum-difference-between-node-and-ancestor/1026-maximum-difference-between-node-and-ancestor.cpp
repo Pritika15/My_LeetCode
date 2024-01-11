6bbbeb90d0982c2f6c8394ed089af67092333036
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-11 18:41:17 +0530

    Time: 75 ms (8.04%), Space: 11 MB (33.87%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -11,32 +11,36 @@
  */
 class Solution {
 public:
-    void Find(TreeNode* root,int &maxi, int &mini)
+    void Find(TreeNode* root, int &maxi, int&mini)
     {
-        if(!root) return ;
+        if(!root) return;
+        
+//         PRe-order
         if(root->val >maxi) maxi=root->val;
-        if(root->val<mini) mini=root->val;
+        if(root->val <mini) mini=root->val;
+        
         if(root->left) Find(root->left,maxi,mini);
         if(root->right) Find(root->right,maxi,mini);
-        
     }
     int maxAncestorDiff(TreeNode* root) {
         if(!root) return 0;
+        int maxi= -1e9, mini=1e9;
+        Find(root,maxi,mini);
         int FinalAns= -1e9;
-       
-
-        int maxi = -1e9, mini = 1e9;
-        Find(root, maxi, mini);
-        FinalAns = max(FinalAns, abs(root->val - maxi));
-        FinalAns = max(FinalAns, abs(root->val - mini));
-
-        if (root->left) {
-            FinalAns = max(FinalAns, maxAncestorDiff(root->left));
+        FinalAns=max(FinalAns,abs(root->val-maxi));
+        FinalAns=max(FinalAns,abs(root->val-mini));
+        
+        if(root->left)
+        {
+            FinalAns=max(FinalAns,maxAncestorDiff(root->left));
         }
-        if (root->right) {
-            FinalAns = std::max(FinalAns, maxAncestorDiff(root->right));
+        
+        if(root->right)
+        {
+            FinalAns=max(FinalAns,maxAncestorDiff(root->right));
         }
-
+        
         return FinalAns;
+        
     }
 };
