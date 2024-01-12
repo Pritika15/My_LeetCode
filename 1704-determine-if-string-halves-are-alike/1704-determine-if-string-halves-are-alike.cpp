da71a66dc6d3c4f6fd4ab9f33731fb48f52f8a83
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-12 20:28:36 +0530

    Time: 3 ms (52.79%), Space: 7 MB (72.68%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,35 +1,34 @@
-#include<bits/stdc++.h> 
 class Solution {
 public:
     bool halvesAreAlike(string s) {
+        int n=s.length();
         set<char>vowels;
         vowels.insert('a');
         vowels.insert('e');
         vowels.insert('i');
         vowels.insert('o');
         vowels.insert('u');
-        // unordered_map<char,int>m1,m2;
-       transform(s.begin(), s.end(), s.begin(), ::tolower);
+        
         int cnt1=0,cnt2=0;
-        for(int i=0;i<(s.length())/2;i++)
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        for(int i=0;i<n/2;i++)
         {
             if(vowels.find(s[i])!=vowels.end())
             {
                 cnt1++;
             }
         }
-        for(int i=(s.length())/2;i<s.length();i++)
+        
+         for(int i=n/2;i<n;i++)
         {
             if(vowels.find(s[i])!=vowels.end())
             {
                 cnt2++;
             }
         }
-        cout<<cnt1<<" "<<cnt2;
-       if(cnt1!=cnt2) return false;
-     
-
-        return true;
         
+        if(cnt1!=cnt2) return false;
+        
+        return true;
     }
 };
