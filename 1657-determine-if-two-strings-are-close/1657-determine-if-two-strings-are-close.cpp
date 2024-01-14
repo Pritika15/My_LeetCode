fd21a525e21232944050c73c10814c0150b1cd8d
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-14 18:34:56 +0530

    Time: 67 ms (88.98%), Space: 20.9 MB (89.19%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
+        
+        for(int i=0;i<word1.length();i++)
+        {
+            v1[word1[i]-'a']++;
+        }
+        
+        for(int i=0;i<word2.length();i++)
+        {
+            v2[word2[i]-'a']++;
+        }
+        
+        for(int i=0;i<v1.size();i++)
+        {
+            if(v1[i]>0)
+            {
+                if(v2[i]<=0) return false;
+            }
+        }
+        
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        
+        return v1==v2;
+    }
+};
