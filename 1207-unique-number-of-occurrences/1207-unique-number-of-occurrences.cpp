afc447d4046da4f1c93ae32d147544da5712a0b9
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-17 20:02:36 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (41.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>m;
-        for(auto it:arr)
+        for(int i=0;i<arr.size();i++)
         {
-            m[it]++;
+            m[arr[i]]++;
         }
+        
         set<int>s;
+        
         for(auto it:m)
         {
             if(s.find(it.second)!=s.end()) return false;
             s.insert(it.second);
         }
+        
         return true;
     }
 };
