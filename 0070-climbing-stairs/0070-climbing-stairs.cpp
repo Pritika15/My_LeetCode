712b8fef36413d03680f587e5f8db1da2bd6f5a2
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-18 21:07:16 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (35.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int f(int n,vector<int>&dp)
-    {
-        if(n==0 || n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=f(n-1,dp)+f(n-2,dp);
-    }
+    // int f(int n,vector<int>&dp)
+    // {
+//     Dp solution:Memoization: top-down appraoch 
+    //     if(n==0 || n==1) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=f(n-1,dp)+f(n-2,dp);
+    // }
     int climbStairs(int n) {
 //         recursive code
-//         Dp solution
-        vector<int>dp(n+1,-1);
-        return f(n,dp);
+//         
+//         Tabulation:Bottom up approach 
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+        // return f(n,dp);
     }
     
 //     O(2^N)
