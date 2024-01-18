2abdb1576c701beb7b4d2b830c8f4dd7f217cf77
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-18 21:03:29 +0530

    Time: 2 ms (45.07%), Space: 6.7 MB (18.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-//     int climbStairdHelper(int n, vector<int>&dp)
-//     {
-// //         if(n==0 || n==1)
-// //             return 1;
-        
-// //         if(dp[n]!= -1)
-// //             return dp[n];
-        
-// //         return climbStairs(n-1)+climbStairs(n-2);
-//         // dp[0]=1;
-//         // dp[1]=1;
-//         // for(int i=2;i<=n;i++)
-//         // {
-//         //     dp[i]=dp[i-1]+dp[i-2];
-//         // }
-//         // return dp[n];
-//     }
-    int climbStairs(int n) {
-        
-        // vector<int>dp(n+1, -1);
-        // return climbStairdHelper(n,dp);
-        
-        int prevstep=1;
-        int prev1step=1;
-        
-        for(int i=2;i<=n;i++)
-        {
-            int curr=prevstep+prev1step;
-            prevstep=prev1step;
-            prev1step=curr;
-        }
-        return prev1step;
+    int f(int n,vector<int>&dp)
+    {
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp)+f(n-2,dp);
     }
+    int climbStairs(int n) {
+//         recursive code
+//         Dp solution
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
+    
+//     O(2^N)
 };
