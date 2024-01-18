1f22826cd77787159fae9490692e66d03302c035
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-18 16:40:19 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (35.64%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int climbStairdHelper(int n, vector<int>&dp)
+    {
+//         if(n==0 || n==1)
+//             return 1;
+        
+//         if(dp[n]!= -1)
+//             return dp[n];
+        
+//         return climbStairs(n-1)+climbStairs(n-2);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        
+        vector<int>dp(n+1, -1);
+        return climbStairdHelper(n,dp);
+    }
+};
