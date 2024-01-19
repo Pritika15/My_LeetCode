436f1dc1de309602e0a9c81722801210a4f1ce59
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-20 00:15:43 +0530

    Time: 15 ms (39.09%), Space: 13.1 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,94 +1,56 @@
 class Solution {
 public:
-//     int solve(int i, int j,vector<vector<int>>& matrix,vector<vector<int>>&dp)
+//     int solve(int i, int j, vector<vector<int>>& matrix,vector<vector<int>>&dp)
 //     {
-//         if(j<0 || j>=matrix[0].size())
-//             return 1e8;
-//         if(i==0)
-//             return matrix[i][j];
-//         if(dp[i][j]!= -1)
-//             return dp[i][j];
+// //         Edge cases
         
-//             int up=matrix[i][j]+solve(i-1,j,matrix,dp);
-//             int leftD=matrix[i][j]+solve(i-1,j-1,matrix,dp);
-//             int rightD=matrix[i][j]+solve(i-1,j+1,matrix,dp);
-           
-            
+//         if(j<0 || j>=matrix.size()) return 1e9;
+// //         Termination case
+//         if(i==0) return matrix[0][j];
         
-//         return dp[i][j]=min(up,min(rightD,leftD));
+// //         memoization
+//         if(dp[i][j]!=-1) return dp[i][j];
+// //         Recursive function
+//         int UpperLD=matrix[i][j]+solve(i-1,j-1,matrix,dp);
+//         int Upper=matrix[i][j]+solve(i-1,j,matrix,dp);
+//         int UpperRD=matrix[i][j]+solve(i-1,j+1,matrix,dp);
+        
+//         return dp[i][j]=min(UpperLD,min(Upper,UpperRD));
 //     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-//         int mini=1e8;
-//         vector<vector<int>>dp(m,vector<int>(n, 0));
+        int mini=1e9;
+        int n=matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        // for(int j=0;j<n;j++)
+        // {
+        //     mini=min(mini,solve(n-1,j,matrix,dp));
+        // }
+        // return mini;
         
-//         // for(int j=0;j<n;j++)
-//         // {
-//         //     mini=min(mini,solve(m-1,j,matrix,dp));
-//         // }
-//         // return mini;
-//         for(int j=0;j<n;j++)
-//         {
-//             dp[0][j]=matrix[0][j];
-//         }
-        
-//         for(int i=1;i<m;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {
-               
-//                 int leftD=1e9,rightD=1e9;
-//                 int up=matrix[i][j]+dp[i-1][j];
-                 
-//                 if(j-1 >=0)
-//                 {
-//                     leftD=matrix[i][j]+dp[i-1][j-1];
-//                 }
-                
-               
-//                    if(j+1<n)
-//                    {
-//                        rightD=matrix[i][j]+dp[i-1][j+1];
-//                    }
-                    
-//                    dp[i][j] =min(up,min(leftD,rightD));
-               
-                
-//             }
-//         }
-//         mini=1e8;
-//         for(int j=0;j<n;j++)
-//         {
-//             mini=min(mini,dp[m-1][j]);
-//         }
-//         return mini;
-        vector<int>prev(n,0);
         for(int j=0;j<n;j++)
         {
-            prev[j]=matrix[0][j];
+            dp[0][j]=matrix[0][j];
         }
-        for(int i=1;i<m;i++)
+        
+        for(int i=1;i<n;i++)
         {
-            vector<int>curr(n,0);
             for(int j=0;j<n;j++)
             {
-                int leftD=1e9,rightD=1e9;
-                int up=matrix[i][j]+prev[j];
-                if(j-1>=0)
-                    leftD=matrix[i][j]+prev[j-1];
                 
-                if(j+1<n)
-                    rightD=matrix[i][j]+prev[j+1];
-                curr[j]=min(up,min(leftD,rightD));
+                int Upper=matrix[i][j]+dp[i-1][j];
+                int UpperRD=1e9,UpperLD=1e9;
+                if(j<n-1) UpperRD=matrix[i][j]+dp[i-1][j+1];
+                if(j>0) UpperLD=matrix[i][j]+dp[i-1][j-1];
+                
+                dp[i][j]=min(Upper,min(UpperRD,UpperLD));
             }
-            prev=curr;
         }
-        int mini=1e9;
+        
         for(int j=0;j<n;j++)
         {
-            mini=min(mini,prev[j]);
+            mini=min(mini,dp[n-1][j]);
         }
+        
         return mini;
     }
 };
