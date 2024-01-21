3aa06e983fe826de5955a5f3096eb48d9ede9f95
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-21 19:48:35 +0530

    Time: 4 ms (25.45%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,20 +16,38 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,0);
-        // return solve(n-1,nums,dp);
-        dp[0]=nums[0];
+//         vector<int>dp(n,0);
+//         // return solve(n-1,nums,dp);
+//         dp[0]=nums[0];
         
-        for(int i=1;i<n;i++)
-        {
-            int take= nums[i];
-            if(i>1) take+=dp[i-2];
-            int notTake=0+dp[i-1];
+//         for(int i=1;i<n;i++)
+//         {
+//             int take= nums[i];
+//             if(i>1) take+=dp[i-2];
+//             int notTake=0+dp[i-1];
             
-            dp[i]=max(take,notTake);
-        }
+//             dp[i]=max(take,notTake);
+//         }
         
-        return dp[n-1];
+//         return dp[n-1];
+        
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        
+        int prev2=nums[0]; //dp[i-2];
+        int prev=max(nums[0],nums[1]); // dp[i-1];
+        
+        for(int i=2;i<n;i++)
+        {
+            int take=nums[i]+prev2;
+            int notTake=prev;
+            
+            int curr=max(take,notTake);
+            
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
         
     }
 };
