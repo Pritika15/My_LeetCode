3d0617abb9ae2db2f6152a09f2fc1fabef1e50b7
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-21 19:45:00 +0530

    Time: 4 ms (25.45%), Space: 9.3 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-//     int solve(int i,vector<int>& nums,vector<int>&dp)
-//     {
-//         if(i==0) return nums[0];
-//         if(i<0) return 0;
-//         if(dp[i]!= -1) return dp[i];
-//         // int take=INT_MIN;
-//         int take=nums[i]+solve(i-2,nums,dp);
-//         int notTake=0+solve(i-1,nums,dp);
-//         return dp[i]=max(take,notTake);
+    int solve(int indx,vector<int>& nums,vector<int>&dp)
+    {
+        if(indx<0) return -1e9;
+//         Base Case
+        if(indx==0) return nums[0];
         
-//     }
+        if(dp[indx]!=-1) return dp[indx];
+//         Recursive function
+        int take= -1e9;
+        if(indx>1) take=nums[indx]+solve(indx-2,nums,dp);
+        int notTake=0+solve(indx-1,nums,dp);
+        
+        return dp[indx]= max(take,notTake);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        // vector<int>dp(n+1,-1);
-        // return solve(n-1,nums,dp);
         vector<int>dp(n,0);
+        // return solve(n-1,nums,dp);
         dp[0]=nums[0];
+        
         for(int i=1;i<n;i++)
         {
-            int take=nums[i];
+            int take= nums[i];
             if(i>1) take+=dp[i-2];
-            int notTake=dp[i-1];
+            int notTake=0+dp[i-1];
             
             dp[i]=max(take,notTake);
         }
+        
         return dp[n-1];
+        
     }
 };
