d97833baca4dde46fbe0c4be9d9e50fcf97fb899
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-22 18:34:07 +0530

    Time: 47 ms (35.68%), Space: 34.5 MB (8.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int>ans;
         unordered_set<int>s;
+        int missing,duplicate;
         for(int i=0;i<nums.size();i++)
         {
-            if(s.find(nums[i])!=s.end()) ans.push_back(nums[i]);
+            if(s.find(nums[i])!=s.end())
+            {
+                duplicate=nums[i];
+            }
             s.insert(nums[i]);
         }
+        
         for(int i=1;i<=nums.size();i++)
         {
             if(s.find(i)==s.end())
-            {ans.push_back(i); break;}
+            {
+                missing=i;
+            }
         }
-       
-        return ans;
+        
+        return {duplicate,missing};
     }
 };
