32793333f8379dbdc32f27ff52697520cc19bcf8
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-24 23:42:09 +0530

    Time: 240 ms (93.57%), Space: 178.5 MB (96.51%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -11,15 +11,15 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root, int count ) {
-        if (!root) return 0;
-        count ^= 1 << (root->val - 1);
-        int res = solve(root->left, count) + solve(root->right, count);
-        if (root->left == root->right && (count & (count - 1)) == 0) res++;
+    int solve(TreeNode* root,int cnt)
+    {
+        if(!root) return 0;
+        cnt^=1<<(root->val-1);
+        int res=solve(root->left,cnt)+solve(root->right,cnt);
+        if(root->left==root->right && (cnt & (cnt-1))==0) res++;
         return res;
     }
     int pseudoPalindromicPaths (TreeNode* root) {
         return solve(root,0);
-        
     }
 };
