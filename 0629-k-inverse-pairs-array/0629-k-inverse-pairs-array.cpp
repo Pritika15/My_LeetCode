cb373c90bfa3eebcec4b39f84ae66328ee16a1f5
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-27 23:22:54 +0530

    Time: 2851 ms (5.49%), Space: 11.5 MB (43.41%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 private:
     const int mod=int(1e9+7);
+    int dp[1001][1001];
+    int f(int n,int k) {
+        //base case
+        if(k<=0) return k==0;
+        if(dp[n][k]!=-1) return dp[n][k];
+
+
+        int ans=0;
+        for(int i=0;i<n;++i) {
+            ans+=f(n-1,k-i);
+            ans%=mod;
+        }
+        return dp[n][k]=ans;
+    }
 public:
     int kInversePairs(int n, int k) {
-        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
-        //base case
-        for(int N=0;N<=n;++N) dp[N][0]=1;
-        for(int N=1;N<=n;++N) {
-            for(int K=0;K<=k;++K) {
-                int ans=0;
-                for(int i=0;i<min(N,K+1);++i) {
-                    ans+=dp[N-1][K-i];
-                    ans%=mod;
-                }
-                dp[N][K]=ans;
-            }
-        }
-        return dp[n][k];
+        memset(dp,-1,sizeof(dp));
+        return f(n,k);
     }
 };
