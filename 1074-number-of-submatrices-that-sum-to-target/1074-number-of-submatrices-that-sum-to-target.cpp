750ccff63f160554f89e00cfea72c484f145e0a7
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-28 23:31:10 +0530

    Time: 399 ms (95.82%), Space: 98.1 MB (74.69%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int res=0;
+        int m=matrix.size();
+        int n=matrix[0].size();
         
-        int res = 0, m = matrix.size(), n = matrix[0].size();
-        for (int i = 0; i < m; i++)
-            for (int j = 1; j < n; j++)
-                matrix[i][j] += matrix[i][j - 1];
-
-        unordered_map<int, int> counter;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                counter = {{0,1}};
-                int cur = 0;
-                for (int k = 0; k < m; k++) {
-                    cur += matrix[k][j] - (i > 0 ? matrix[k][i - 1] : 0);
-                    res += counter.find(cur - target) != counter.end() ? counter[cur - target] : 0;
-                    counter[cur]++;
+//         Prefix Sum
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                matrix[i][j]+=matrix[i][j-1];
+            }
+        }
+        
+//         Unoredered Map : Prefix Sum ---> frequency 
+        unordered_map<int,int>mappy;
+//         To explore all the possible combinations
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                mappy={{0,1}};
+                int curr=0;
+                
+                for( int k=0;k<m;k++)
+                {
+                    curr+=matrix[k][j]-(i>0 ? matrix[k][i-1]:0);
+                    res+=mappy.find(curr-target)!=mappy.end() ? mappy[curr-target]:0;
+                    mappy[curr]++;
                 }
             }
         }
         return res;
+        
     }
-    
 };
