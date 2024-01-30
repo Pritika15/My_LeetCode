9b547d509dffaccc83884b03d0e96e64adbb0377
Pritika Sharma <psharma6_be20@thapar.edu>
2024-01-30 23:17:15 +0530

    Time: 4 ms (94.03%), Space: 15.5 MB (10.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int>s;
-         for(int i=0;i<tokens.size();i++){
-            if(tokens[i]=="+" or tokens[i]=="-" or tokens[i]=="*" or tokens[i]=="/"){
+        for(int i=0;i<tokens.size();i++)
+        {
+            if(tokens[i]=="+" or tokens[i]=="-" or tokens[i]=="*" or tokens[i]=="/" )
+            {
+//                 operator ---> pop out the last 2 eles : apply that operator and push back the result;
                 int op2=s.top();
                 s.pop();
                 int op1=s.top();
                 s.pop();
-                // switch(t[i][0]){
-                //     case '+':
-                //         s.push(op1+op2);
-                //         break;
-                //     case '-':
-                //         s.push(op1-op2);
-                //         break;
-                //     case '*':
-                //         s.push(op1*op2);
-                //         break;
-                //     case '/':
-                //         s.push(op1/op2);
-                //         break;
+                
                 if(tokens[i][0]=='+') s.push(op1+op2);
                 else if(tokens[i][0]=='-') s.push(op1-op2);
                 else if(tokens[i][0]=='*') s.push(op1*op2);
                 else s.push(op1/op2);
-                // }
+//                     4+5===== 45+
             }
-            else{
+            else
+            {
                 s.push(stoi(tokens[i]));
             }
         }
