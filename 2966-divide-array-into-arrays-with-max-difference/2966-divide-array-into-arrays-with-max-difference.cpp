b6c32cc5d138e542a2d6939ae72e260caf9db94e
Pritika Sharma <psharma6_be20@thapar.edu>
2024-02-01 23:16:09 +0530

    Time: 143 ms (91.51%), Space: 119.6 MB (39.25%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>> ans;
-    sort(nums.begin(), nums.end());
-    for(int i = 2; i < nums.size(); i += 3){
-        if(nums[i] - nums[i - 2] > k) return {};
-        ans.push_back({nums[i-2], nums[i-1], nums[i]});
-    }
-    return ans;
+        vector<vector<int>>  ans;
+//         step 1: sort the given arr
+        sort(nums.begin(),nums.end());
+//         step2 : check for the 3 consecutive places
+        for(int i=2;i<nums.size();i=i+3)
+        {
+            if(nums[i]-nums[i-2]>k) return {};
+            ans.push_back({nums[i-2],nums[i-1],nums[i]});
+        }
+        return ans;
     }
 };
