bc78e6e6f25e2bc3921ae0f6210b644d6f584c1b
Pritika Sharma <psharma6_be20@thapar.edu>
2024-02-04 11:12:52 +0530

    Time: 20 ms (39.96%), Space: 10.4 MB (12.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-          vector<int> map(128,0);
-        for (char c : t) {
-            map[c]++;
-        }
-
-        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
-        while (end < s.size()){
-            if (map[s[end++]]-- > 0) {
-                counter--;
+        unordered_map<char, int> letters; //unordered map for storing the characters in t that we need to check for in s
+        for(auto c : t) letters[c]++; 
+        int count = 0; //counts number of t's letters in current window
+        int low = 0, min_length = INT_MAX, min_start = 0;     
+        for(int high = 0; high<s.length(); high++) {
+            if(letters[s[high]] > 0) count++; //means that this letter is in t   
+            letters[s[high]]--; //reduce the count for the letter on which we are currently 
+            if(count == t.length()) { //if current windows contains all of the letters in t
+                while(low < high && letters[s[low]] < 0) letters[s[low]]++, low++; //move low ahead if its not of any significance
+                if(min_length > high-low) min_length = high-(min_start=low)+1; //update the min length
+                letters[s[low++]]++; //move low ahaead and also increment the value
+                count--; //count-- as we are moving low ahead & low pointed to a char in t before
             }
-            while (counter == 0) {
-                if (end - begin < d) {
-                    head = begin;
-                    d = end - head;
-                }
-                if (map[s[begin++]]++ == 0) {
-                    counter++;
-                }
-            }  
         }
-        return d == INT_MAX ? "" : s.substr(head, d);
+        return min_length == INT_MAX ? "" : s.substr(min_start, min_length); //check for edge case & return the result
     }
 };
