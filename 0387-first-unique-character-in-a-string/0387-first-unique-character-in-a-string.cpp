23bdbefd57f5898a8203c41283b7f5fe1d549a45
Pritika Sharma <psharma6_be20@thapar.edu>
2024-02-05 19:23:55 +0530

    Time: 16 ms (95.41%), Space: 12 MB (24.32%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+//     step 1
         vector<int>v(26,0);
+        int ans= -1;
         for(int i=0;i<s.length();i++)
         {
             v[s[i]-'a']++;
         }
-        int ans= -1;
+        
+//         traverse over the str s && use the vector of freq t get the first indeex
         for(int i=0;i<s.length();i++)
         {
             if(v[s[i]-'a']==1)
-            {
-                ans= i;
-                break;
+            {ans=i;
+             break;
             }
         }
+        
         return ans;
     }
 };
