0283add8c47a67e692d24ed035831d6f3873d811
Pritika Sharma <psharma6_be20@thapar.edu>
2026-05-24 14:48:26 +0530

    Time: 32 ms (7.15%), Space: 22 MB (5.01%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0=1;
-        for(int i=0;i<matrix.size();i++)
+        vector<pair<int,int>> indices;
+        for(int i =0;i<matrix.size();i++)
         {
-            for(int j=0;j<matrix[0].size();j++)
+            for(int j = 0 ;j<matrix[0].size();j++)
             {
-                if(matrix[i][j]==0)
-                {
-                    matrix[i][0]=0;
-                    
-                    if(j!=0) matrix[0][j]=0;
-                    else col0=0;
-                
-                }
+                if(matrix[i][j]==0) indices.push_back({i,j});
             }
         }
-        
-        for(int i=1;i<matrix.size();i++)
+
+        for(int i =0;i<indices.size();i++)
         {
-            for(int j=1;j<matrix[0].size();j++)
+            int row = indices[i].first;
+            int col = indices[i].second;
+            for(int j =0;j<matrix[0].size();j++)
             {
-                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
-                
+                matrix[row][j] = 0;
             }
+            for(int k = 0;k<matrix.size();k++)
+            {
+                matrix[k][col]=0;
+            }
+            
         }
-        if(matrix[0][0]==0)
-        {
-            for(int j=0;j<matrix[0].size();j++) matrix[0][j]=0;
-        }
-        if(col0==0)
-        {
-            for(int i=0;i<matrix.size();i++) matrix[i][0]=0;
-        }
-        
     }
 };
