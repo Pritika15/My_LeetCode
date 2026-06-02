215967c5993acd262a2b927f56fe926000cfddd4
Pritika Sharma <psharma6_be20@thapar.edu>
2026-06-02 13:28:37 +0530

    Time: 0 ms (100%), Space: 9 MB (53.77%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char>st;
+        int depth = 0;
         int n = s.length();
-        vector<pair<int,int>>v;
-        st.push(s[0]);
-        int start = 0;
-        string ans ="";
-
-        for(int i =1;i<n;i++)
-        {
-            if(s[i] == ')')
-            {
-                st.pop();
-                if(st.empty() && i<n)
-                {
-                    v.push_back({start,i});
-                    start = i+1;
-
-                } 
-            }
-            else{st.push(s[i]);}
-        }
-
-        for( auto ptr: v)
-        {
-            s[ptr.first] = 'a';
-            s[ptr.second] = 'a'; 
-        }
+        string ans = "";
 
         for(int i =0;i<n;i++)
         {
-            if(s[i]!='a') ans.push_back(s[i]);
+            if(s[i]=='(')
+            {
+                if(depth>0) ans.push_back(s[i]);
+                depth++;
+
+            } 
+            else
+            {
+                depth--; 
+                if(depth>0) ans.push_back(s[i]);
+
+            } 
+            
         }
         return ans;
+        
     }
 };
