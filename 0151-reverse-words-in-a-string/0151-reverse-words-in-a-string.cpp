c2e5c6811168ef3c0ca0c3859f8ea66701d6df38
Pritika Sharma <psharma6_be20@thapar.edu>
2026-06-02 14:25:40 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string>st;
+        // Step 1: Reverse the entire string first
+        reverse(s.begin(), s.end());
+        
         int n = s.length();
-        string temp = "";
-        for(int i =0;i<n;i++)
-        {
-            if(s[i]!=' ' || (i==n-1 && s[i]!=' '))
-            {
-                temp.push_back(s[i]);
-            }
-            else
-            {
-                if(!temp.empty())st.push(temp);
-                temp = "";
+        int idx = 0; // The 'write' pointer to compact the string in-place
+        
+        for (int i = 0; i < n; i++) {
+            // Skip any spaces to find the beginning of a word
+            if (s[i] != ' ') {
+                // If this isn't the first word, append a single space spacer
+                if (idx != 0) {
+                    s[idx++] = ' ';
+                }
+                
+                // Keep track of where this specific word starts in our clean area
+                int start = idx;
+                
+                // Copy the entire word forward to the write pointer
+                while (i < n && s[i] != ' ') {
+                    s[idx++] = s[i++];
+                }
+                
+                // Reverse this individual word back to its correct orientation
+                reverse(s.begin() + start, s.begin() + idx);
             }
         }
-        if(!temp.empty())st.push(temp);
-        temp = "";
-        while(!st.empty())
-        {
-            temp+=st.top();
-            st.pop();
-            temp.push_back(' ');
-        }
-        temp.pop_back();
-        return temp;
+        
+        // Step 2: Crop off all trailing extra spaces left at the back
+        s.resize(idx);
+        return s;
     }
 };
