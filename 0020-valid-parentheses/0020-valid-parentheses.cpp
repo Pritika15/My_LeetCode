ffb07669abc1dffd9fb4d595c950d8e4d2bab6c1
Pritika Sharma <psharma6_be20@thapar.edu>
2026-08-19 12:18:48 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n=s.length();
-        stack<char>s1;
-        for(int i=0;i<n;i++)
+         stack<char>s1;
+        bool ans = true;
+        for(int i =0;i<s.length();i++)
         {
-            if(s[i]=='(' || s[i]=='{' ||s[i]=='[')
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
             {
                 s1.push(s[i]);
             }
-            else if((s[i]==')' || s[i]==']' || s[i]=='}' ) && s1.empty())
-            {
-                return false;
-            }
             else
             {
-                if(!s1.empty())
-                {
-                    char temp=s1.top();
-                    s1.pop();
-                    if(s[i]==')')
-                    {
-                        if(temp=='[' || temp=='{')
-                            return false;
-                    }
-                    else if(s[i]==']')
-                    {
-                        if(temp=='(' || temp=='{')
-                            return false;
-                    }
-                    else
-                    {
-                        if(temp=='(' || temp=='[')
-                            return false;
-                    }
-                }
+                if (s1.empty()) return false;
+                char top = s1.top();
+                if((top =='(' && s[i]!=')' )|| (top=='[' && s[i]!=']') || (top=='{' && s[i]!='}') ) return false;
+                s1.pop();
             }
         }
         if(!s1.empty()) return false;
-        
-        return true;
+        return ans;
     }
 };
